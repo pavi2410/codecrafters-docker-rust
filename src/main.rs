ccbfa30f7e42e3e185c378984e530dc7bafcc8b8
Pavitra Golchha <hello@pavi2410.me>
2023-11-23 15:32:10 +0530

    stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Result};
+use std::process::Stdio;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
@@ -7,6 +8,8 @@ fn main() -> Result<()> {
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
         .args(command_args)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .output()
         .with_context(|| {
             format!(
@@ -15,10 +18,7 @@ fn main() -> Result<()> {
             )
         })?;
     
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
-    } else {
+    if !output.status.success() {
         std::process::exit(1);
     }
 
