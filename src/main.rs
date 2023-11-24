fd2f04a36ae562f571bef06ea92bc8c4d256eb22
Pavitra Golchha <hello@pavi2410.me>
2023-11-24 11:36:28 +0530

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use anyhow::{Context, Result};
 use std::process::Stdio;
+use std::os::unix::fs;
+use tempfile::tempdir;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
+    let tmp_dir = tempdir()?;
+    fs::chroot(tmp_dir)?;
+    std::env::set_current_dir("/")?;
+
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
